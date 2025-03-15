dcb3859d534c30f150c48110304461ba71088875
busragiran <bgiranzumrut@academic.rrc.ca>
2025-03-15 12:44:38 -0500

    Added Hierarchical Search

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -3,6 +3,8 @@ class BooksController < ApplicationController
     @genres = Book.distinct.pluck(:genre)
 
     @books = Book.all
+
+    # Apply search query
     if params[:query].present?
       if ActiveRecord::Base.connection.adapter_name.downcase.start_with?('sqlite')
         @books = @books.where("title LIKE ?", "%#{params[:query]}%")  # SQLite
@@ -11,10 +13,12 @@ class BooksController < ApplicationController
       end
     end
 
-    @books = @books.where(genre: params[:genre]) if params[:genre].present? && params[:genre] != ""
+    # Apply hierarchical filtering (filter by genre)
+    if params[:genre].present? && params[:genre] != "All Genres"
+      @books = @books.where(genre: params[:genre])
+    end
 
+    # Paginate results
     @books = @books.page(params[:page]).per(10)
-
-
   end
 end
