526280885c2b7bc747b41444f850b752d78edbbf
busragiran <bgiranzumrut@academic.rrc.ca>
2025-03-15 13:08:06 -0500

    Update Authors show page to  get all bookd

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -1,4 +1,9 @@
 class AuthorsController < ApplicationController
+  def show
+    @author = Author.find(params[:id])
+    @books = @author.books  # Get all books written by this author
+  end  # Get all books written by this author
+
   def index
     if params[:query].present?
       @authors = Author.where("name ILIKE ?", "%#{params[:query]}%")
