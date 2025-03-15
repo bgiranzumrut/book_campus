32d2ee879bd4253e8caff2156d2da06fd8eceea6
busragiran <bgiranzumrut@academic.rrc.ca>
2025-03-15 12:36:10 -0500

    Added simple search both books and authors

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -1,5 +1,11 @@
 class AuthorsController < ApplicationController
   def index
-    @authors = Author.page(params[:page]).per(10)
+    if params[:query].present?
+      @authors = Author.where("name ILIKE ?", "%#{params[:query]}%")
+    else
+      @authors = Author.all
+    end
+
+    @authors = @authors.page(params[:page]).per(10)
   end
 end
--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,11 +1,20 @@
 class BooksController < ApplicationController
   def index
-    if params[:genre].present?
-      @books = Book.where(genre: params[:genre])
-    else
-      @books = Book.all
+    @genres = Book.distinct.pluck(:genre)
+
+    @books = Book.all
+    if params[:query].present?
+      if ActiveRecord::Base.connection.adapter_name.downcase.start_with?('sqlite')
+        @books = @books.where("title LIKE ?", "%#{params[:query]}%")  # SQLite
+      else
+        @books = @books.where("title ILIKE ?", "%#{params[:query]}%") # PostgreSQL
+      end
     end
+
+    @books = @books.where(genre: params[:genre]) if params[:genre].present? && params[:genre] != ""
+
     @books = @books.page(params[:page]).per(10)
-    @genres = Book.distinct.pluck(:genre)  # Get unique genres for the filter
+
+
   end
 end
